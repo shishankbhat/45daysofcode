52470277de5bbe76d6572b27b4257b35047d86d9
shishankbhat <116367979+shishankbhat@users.noreply.github.com>
2023-09-24 16:38:24 +0530

    Create DAT7.cpp

---
+++ DAT7.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,c;
+	    cin >> a >> b >>c;\
+	    if (b > a+c){
+	        
+	        cout << b <<endl;
+	    }
+	    else{
+	        cout << a + c<<endl;
+	    }
+	}
+	return 0;
+}
